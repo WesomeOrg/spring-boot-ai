d2a1661c2d15605de1a02fae58c756d423950870
WesomeOrg <wesome.org@gmail.com>
2026-01-10 16:13:06 +0530

    hello spring ai

--- Hello Spring Ai/entrypoint.sh
+++ Hello Spring Ai/entrypoint.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
-# Start Ollama in the background.
-/bin/ollama serve &
-# Record Process ID.
-pid=$!
-# Pause for Ollama to start.
-sleep 5
-# The default Ollama Model in Spring Ai is mistral, but it can be changed in the applications property file. Make sure to download the same Model here
-echo "🔴 Retrieve llama3.1 model..."
-ollama pull mistral
-echo "🟢 Done!"
-# Wait for the Ollama process to finish.
-wait $pid
+##!/bin/bash
+## Start Ollama in the background.
+#/bin/ollama serve &
+## Record Process ID.
+#pid=$!
+## Pause for Ollama to start.
+#sleep 5
+## The default Ollama Model in Spring Ai is mistral, but it can be changed in the applications property file. Make sure to download the same Model here
+#echo "🔴 Retrieve llama3.1 model..."
+#ollama pull llama3.1:latest
+#echo "🟢 Done!"
+## Wait for the Ollama process to finish.
+#wait $pid
